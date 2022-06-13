8b6c841f855539d6235ce6a7e3197757eda5d68f
Ferran Casals <ferrancasalsfajula@gmail.com>
2022-06-13 16:29:04 +0200

    JWT Generator

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require('../models/userModel');
+const generateToken = require('../utils/generateToken');
 
 
 const registerUser = asyncHandler(async (req,res) => {
@@ -16,7 +17,8 @@ const registerUser = asyncHandler(async (req,res) => {
                 name: newUser.name,
                 email: newUser.email,
                 isAdmin: newUser.isAdmin,
-                pic: newUser.pic
+                pic: newUser.pic,
+                token: generateToken(newUser._id)
             }})
         }    
     } catch(err){
@@ -39,7 +41,8 @@ const authUser = asyncHandler(async (req,res) => {
                     name: user.name,
                     email: user.email,
                     isAdmin: user.isAdmin,
-                    pic: user.pic
+                    pic: user.pic,
+                    token: generateToken(user._id)
                 }})
             } else {
                 res.status(400).json({message: 'Invalid password'})
